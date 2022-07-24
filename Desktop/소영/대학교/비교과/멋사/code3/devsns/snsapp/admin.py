96c524894fc4601599fb885e4064ba445d2cb5b0
place0 <thdud030101@naver.com>
2022-07-24 23:26:53 +0900

    sy

--- Desktop/소영/대학교/비교과/멋사/code3/devsns/snsapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Comment, FreePost, FreeComment
-
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(FreePost)
-admin.site.register(FreeComment)
